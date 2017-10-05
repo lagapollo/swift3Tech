bd27008d03a7dd9177eb3f5468acbff65d410489
Sylvain Laure <slaure.extern@groupeseb.com>
2017-10-05 09:11:08 +0200

    framework swift 3

---
+++ swift3Tech/swift3Framework/swift3.swift
@@ -0,0 +1,14 @@
+//
+//  swift3.swift
+//  swift3Framework
+//
+//  Created by Sylvain Laure on 05/10/2017.
+//  Copyright © 2017 Sylvain Laure. All rights reserved.
+//
+
+import Foundation
+
+
+public class swift3 {
+    let helloWorld = "hello world from swift 3"
+}
---
+++ swift3Tech/swift3Framework/swift3Framework.h
@@ -0,0 +1,19 @@
+//
+//  swift3Framework.h
+//  swift3Framework
+//
+//  Created by Sylvain Laure on 05/10/2017.
+//  Copyright © 2017 Sylvain Laure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for swift3Framework.
+FOUNDATION_EXPORT double swift3FrameworkVersionNumber;
+
+//! Project version string for swift3Framework.
+FOUNDATION_EXPORT const unsigned char swift3FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift3Framework/PublicHeader.h>
+
+
